b9697f2241099d2fe86f9a728713ce6911fbde04
stdio2016 <randomfrank8@gmail.com>
2018-09-27 12:34:09 +0800

    move edit button

--- application/views/anncs/detail.php
+++ application/views/anncs/detail.php
@@ -1,10 +1,12 @@
 <section>
         <h1 style="word-wrap:break-word;"><?php if (isset($annc->title)) echo htmlspecialchars($annc->title); ?></h1>
-        <?php if (isset($annc->date)) echo $annc->date; ?><br><br>
-        <?php if (isset($annc->description)) echo $annc->description; ?>
+        <?php if (isset($annc->date)) echo $annc->date; ?>
         <?php if (Auth::isAdmin()) { ?>
           <p>
             <button class="blue button" type="button" onclick='location.href="<?php echo URL; ?>anncs/edit/<?= $annc->id ?>";'>編輯公告</button>
           </p>
+        <?php } else { ?>
+          <br><br>
         <?php } ?>
+        <?php if (isset($annc->description)) echo $annc->description; ?>
 </section>
